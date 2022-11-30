f7398b7c185bf29a7c15e5bc188031fd1b918264
bgriswol <71152751+bgriswol@users.noreply.github.com>
2022-11-30 16:06:52 -0500

    Added bootstrap, bare outline of files here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="App-header">Portfolio</header>
+      <div className="cart">  
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+        </div>
     </div>
   );
 }
