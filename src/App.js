d3e7a1b4fb990e5d3f08ddcfc397a45426756e00
bgriswol <71152751+bgriswol@users.noreply.github.com>
2022-11-30 23:53:25 -0500

    Initial setup; looking good

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
 import './bootstrap.css';
+import './index.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Portfolio</header>
-      <div className="cart">  
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">
+          <h1>Portfolio</h1>
+          <h1>Bridget Griswold</h1>
+          </header>
+      <div class="container">
+        <div class="row" id="shelter-div">
+          <div class="col-5" id="shelter"></div>
+          <div class="col-5">
+            2 of 3 (wider)
+          </div>
         </div>
+      </div>
+      <div class="container">
+        <div class="row" id="storyboard-div">
+          <div class="col-6">
+            2 of 3 (wider)
+          </div>
+          <div class="col-6" id="storyboards"></div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row" id="charity-div">
+          <div class="col-5" id="charity"></div>
+          <div class="col-5">
+            2 of 3 (wider)
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row" id="yhangry-div">
+          <div class="col-6">
+            2 of 3 (wider)
+          </div>
+          <div class="col-6" id="yhangry"></div>
+        </div>
+      </div>
     </div>
   );
 }
