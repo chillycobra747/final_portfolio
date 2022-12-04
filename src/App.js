a4b85ac7953b98b812f8f59fb55ccf545a51823a
bgriswol <71152751+bgriswol@users.noreply.github.com>
2022-12-04 14:10:01 -0500

    Writing added

--- src/App.js
+++ src/App.js
@@ -27,7 +27,10 @@ function App() {
                 <li>Finally, I implemented sorting and filtering functionality, using toggle buttons!</li>
               </h3>
               <h3><h2>Findings </h2>
-                <li>I leared a LOT about react through this project </li>
+                <li>I leared a lot about react through by time building this website. This was my first 
+                  experience working with react. </li>
+                <li>I chose to build an animal shelter to highlight the importance of adopting dogs from
+                   selters, as many dogs are left without homes. </li>
               </h3>
             </div>
             
@@ -43,6 +46,20 @@ function App() {
               tasked to chose and observe user interactions with any physical interface in real life. Through
               these observations, I should note (and then draw) how the user interacted with the interface. This 
               site records my observations, user interview questions, and findings. </h3>
+              <h3><h2>Process </h2>
+                <li>This idea started with observations, in watching individuals interact with the Ceremony Tea 
+                  'Toast' checkout device. </li>
+                <li> Following my observations, I was able to build user personas and depict them through 
+                  drawings and character personas.</li>
+                <li>To learn about how users felt about the checkout process at Ceremony Tea, I 
+                  conducted interviews and recorded my findings on my site. </li>
+              </h3>
+              <h3><h2>Findings </h2>
+                <li>Storyboarding is a wonderful and effective way to track a user's experience with 
+                  a product! </li>
+                <li>Observing is really fun! But experience some awkward moments of people confused
+                  why I was staring at them so much. </li>
+              </h3>
           </div>
           <div class="col-6" id="storyboards"></div>
         </div>
@@ -56,6 +73,20 @@ function App() {
             have previously encountered, which I believed needs improvements. I used a chairty website 
             I had used in high school, and remembered how poor the user experience was. Thus, I built 
             this project to improve upon the currently published Assistance Leauge website. </h3>
+            <h3><h2>Process </h2>
+                <li>I first studied the content and layout of the site I was going to rebuild. Once I 
+                  had a thorough understanding of the website, I could iterate and improve from there. </li>
+                <li> I began my rebuilding process with low-fi and high-fi sketches. </li>
+                <li>I used a navbar to allow for easy navigation between the different pages of the website. </li>
+                <li>My focus for the website redesign was simplification. I aimed to make it easier and faster 
+                  for new users to understand the website's purpose and elements.</li>
+              </h3>
+              <h3><h2>Findings </h2>
+                <li>While time-consuming, creating low-fi and high-fi sketches before building your site  
+                  with code is extremely useful!  </li>
+                <li>Oftentimes, simple is better! I was proud in my ability to make the website more 
+                  effective then the origional, with less mess and fewer words. </li>
+              </h3>
             </div>
         </div>
       </div>
@@ -67,6 +98,21 @@ function App() {
             about, which was yhangry, a British private-chef service. Before viewing the yhangry site, 
             only with knowledge about what the company does, we challenged outselves to create an 
             interactive Figma which could be used as the website of our chosen startup.  </h3>
+            <h3><h2>Process </h2>
+                <li>In order for this project to be successful, we first needed to understand the purpose 
+                  and mission of the company. Thus, we started with thorough research on our chosen startup. </li>
+                <li>My 3 team members and I each created low-fi sketches of what we thought the site should 
+                  look like. From there, we worked together to create a consolidated plan. </li>
+                <li>We worked together on an interactive Figma site, adding pages and navigation between each. </li>
+              </h3>
+              <h3><h2>Findings </h2>
+                <li>Success in a project is going to take many iterations! My team and I learned so much 
+                  from eachother in order to make this website great, and also from feedback from others in 
+                  our UI/UX class.
+                </li>
+                <li>Making iteractive Figmas is an easy and great way to craft a website. Figma has so many 
+                  great features and functionality components I had not known about before this project. </li>
+              </h3>
           </div>
           <div class="col-6" id="yhangry"></div>
         </div>
